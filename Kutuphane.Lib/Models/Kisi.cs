76ea34f39fa306343aa6650e00d226a9a175d6ff
Furkan Kılıç <furkankilicytu@gmail.com>
2018-12-18 23:08:52 +0300

    Add project files.

---
+++ Kutuphane.Lib/Models/Kisi.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kutuphane.Lib.Models
+{
+    public abstract class Kisi
+    {
+        private string Ad { get; set; }
+        private string Soyad { get; set; }
+        private DateTime DogumTarihi { get; set; }
+
+        public override string ToString()
+        {
+            return $"{ this.Ad},{ this.Soyad}";
+        }
+    }
+}
---
+++ Kutuphane.Lib/Models/Ogrenci.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kutuphane.Lib.Models
+{
+    public class Ogrenci : Kisi
+    {
+        private string Telefon { get; set; }
+        private string TCKN { get; set; }
+    }
+}
